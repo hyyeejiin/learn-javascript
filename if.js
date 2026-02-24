2c936ff30efe1109a1983ffdd43daf81ba2d9680
hyyeejiin <hyyeejiin@gmail.com>
2026-02-24 13:13:49 +0900

    오전 수업2

---
+++ if.js
@@ -0,0 +1,19 @@
+console.log("if");
+
+//조건문
+const score = 80;
+
+// if (score >= 60) {
+//     //조건식이 true면 실행할 로직
+//     console.log("합격");
+// }
+
+if (score >= 60) {
+    //조건식이 true면 실행할 로직
+    console.log("합격");
+}else {
+    console.log("불합격");
+    
+}
+
+console.log("조건문 끝");
--- index.js
+++ index.js
@@ -69,4 +69,3 @@ const upperString = (str) => str.toUpperCase() ;
 console.log("대문자 변환 : ",upperString("wlwldkghkdlet"));
 
 
-
