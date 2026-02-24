994c70c8f8c5bd786d5caa8d6dfa51832c3566c1
hyyeejiin <hyyeejiin@gmail.com>
2026-02-24 14:21:18 +0900

    오후 수업1

--- if.js
+++ if.js
@@ -1,19 +1,48 @@
 console.log("if");
 
 //조건문
-const score = 80;
+const score = 96;
 
 // if (score >= 60) {
 //     //조건식이 true면 실행할 로직
 //     console.log("합격");
 // }
 
-if (score >= 60) {
-    //조건식이 true면 실행할 로직
-    console.log("합격");
-}else {
-    console.log("불합격");
-    
+// if (score >= 60) {
+//   //조건식이 true면 실행할 로직
+//   console.log("합격");
+// } else {
+//   console.log("불합격");
+// }
+
+// score >= 60 ? console.log("합격") : console.log("불합격");
+
+console.log( score >=60 ? "합격" :"불합격")
+//console.log("조건문 끝");
+
+//else if
+
+let grade = "F";
+
+if (score >= 90) {
+  grade = "A";
+} else if (score >= 80) {
+  grade = "B";
+} else {
+  grade = "C";
 }
 
-console.log("조건문 끝");
+console.log(`학생의 등급은 ${grade}입니다.`);
+
+// == === != !==
+// false 로 인식 : 0, '', null, undefined
+if(0){
+    console.log("true");
+}else {
+    console.log("false");
+}
+
+const userInput = "" ; // 사용자 입력 값
+if(!userInput){
+    console.log("입금해줘");
+}
